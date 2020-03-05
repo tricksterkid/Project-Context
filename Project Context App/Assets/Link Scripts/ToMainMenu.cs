6af5e310977242a7399c452c3f40a825a1845783
tricksterkid <deighton.zakelijk@gmail.com>
2020-03-05 13:07:14 +0100

    VERSION 2 - Added new links to other scenes.

---
+++ Project Context App/Assets/Link Scripts/ToMainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMainMenu : MonoBehaviour
+{
+
+    public void GoToNextScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
---
+++ Project Context App/Assets/Link Scripts/ToStudent2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToStudent2 : MonoBehaviour
+{
+
+    public void GoToNextScene()
+    {
+        SceneManager.LoadScene("Student2");
+    }
+
+}
