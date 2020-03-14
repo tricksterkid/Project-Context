6743213bff408c2a453bda85a3f109bdbe56cf6a
tricksterkid <deighton.zakelijk@gmail.com>
2020-03-14 17:39:07 +0100

    VERSION 10 - Art implemented. MISSING: start screen art & skills + categories.

--- Project Context App/Assets/Scripts/ChCrHairCorrect.cs
+++ Project Context App/Assets/Scripts/ChCrHairCorrect.cs
@@ -5,15 +5,17 @@ public class ChCrHairCorrect : MonoBehaviour
 {
 
     public Sprite[] Options;
-    public Sprite Option1;
-    public Sprite Option2;
     public Image displayImage;
     public Button Next;
     public Button Previous;
     public GameObject ColorButtons1;
     public GameObject ColorButtons2;
+    public GameObject ColorButtons3;
+    public GameObject ColorButtons4;
     public GameObject HairColor1;
     public GameObject HairColor2;
+    public GameObject HairColor3;
+    public GameObject HairColor4;
     public int i = 0;
 
     public void BtnNext()
@@ -40,21 +42,53 @@ public class ChCrHairCorrect : MonoBehaviour
         {
             ColorButtons1.SetActive(true);
             ColorButtons2.SetActive(false);
+            ColorButtons3.SetActive(false);
+            ColorButtons4.SetActive(false);
             HairColor1.SetActive(true);
             HairColor2.SetActive(false);
+            HairColor3.SetActive(false);
+            HairColor4.SetActive(false);
 
         } else if (i == 2)
         {
             ColorButtons1.SetActive(false);
             ColorButtons2.SetActive(true);
+            ColorButtons3.SetActive(false);
+            ColorButtons4.SetActive(false);
             HairColor1.SetActive(false);
             HairColor2.SetActive(true);
+            HairColor3.SetActive(false);
+            HairColor4.SetActive(false);
+        } else if (i == 3)
+        {
+            ColorButtons1.SetActive(false);
+            ColorButtons2.SetActive(false);
+            ColorButtons3.SetActive(true);
+            ColorButtons4.SetActive(false);
+            HairColor1.SetActive(false);
+            HairColor2.SetActive(false);
+            HairColor3.SetActive(true);
+            HairColor4.SetActive(false);
+        } else if (i == 4)
+        {
+            ColorButtons1.SetActive(false);
+            ColorButtons2.SetActive(false);
+            ColorButtons3.SetActive(false);
+            ColorButtons4.SetActive(true);
+            HairColor1.SetActive(false);
+            HairColor2.SetActive(false);
+            HairColor3.SetActive(false);
+            HairColor4.SetActive(true);
         } else
         {
             ColorButtons1.SetActive(false);
             ColorButtons2.SetActive(false);
+            ColorButtons3.SetActive(false);
+            ColorButtons4.SetActive(false);
             HairColor1.SetActive(false);
             HairColor2.SetActive(false);
+            HairColor3.SetActive(false);
+            HairColor4.SetActive(false);
         }
     }
 
