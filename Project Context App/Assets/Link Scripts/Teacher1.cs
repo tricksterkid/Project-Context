1c36c1034223dde9457aa670b9a1dc2324af5637
tricksterkid <deighton.zakelijk@gmail.com>
2020-03-17 10:48:27 +0100

    VERSION 10.2 - Added teacher part.

---
+++ Project Context App/Assets/Link Scripts/Teacher1.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teacher1 : MonoBehaviour
+{
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Teacher1");
+    }
+
+}
